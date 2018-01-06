07790873d8139687f5dfbe5469464ef4515dc80a
Mykhaylo_Lohvynenko <Mykhaylo_Lohvynenko@epam.com>
2018-01-06 19:10:59 +0200

    Vagrant provision create  user ansible and add ssh key for him

--- shell/user.sh
+++ shell/user.sh
@@ -3,10 +3,12 @@ set -eu
 
 USER="ansible"
 PASSWORD="ansiblepass"
+SSH_HOME="/home/$USER/.ssh"
+SHARE="/vagrant"
 
 # Add user 
 # TODO: Check user  !
-sudo useradd $USER$
+sudo useradd $USER
 
 # Install password
 # sudo echo "$USER:$PASSWORD" | chpasswd
@@ -17,5 +19,8 @@ sudo useradd $USER$
 sudo echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # Copy ssh key for ansible user
-# ssh-copy-id
+sudo -u $USER -H mkdir $SSH_HOME
+sudo cp $SHARE/keys/authorized_keys $SSH_HOME/
 
+sudo chown -R $USER:$USER $SSH_HOME
+sudo chmod 600 $SSH_HOME/authorized_keys
