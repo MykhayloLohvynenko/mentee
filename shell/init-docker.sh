d50c54498171889b7db9b53965caeba6b18108de
Mykhaylo_Lohvynenko <Mykhaylo_Lohvynenko@epam.com>
2018-01-30 11:59:00 +0200

    Using roles

--- shell/init-docker.sh
+++ shell/init-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo ansible-playbook /vagrant/ansible/playbook/docker-cluster-up.yaml
+# sudo ansible-playbook /vagrant/ansible/playbook/docker-cluster-up.yaml
+sudo ansible-playbook /vagrant/ansible/site.yaml
